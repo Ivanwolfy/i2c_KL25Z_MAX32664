b71a245a3ab3cd6098fa9a0847c21b6d35ba5868
ivanwolfy <57159606+Ivanwolfy@users.noreply.github.com>
2020-04-04 00:17:01 -0400

    Update MKL25Z128xxx4_i2c_Manual.c

--- MKL25Z128xxx4_i2c_Manual.c
+++ MKL25Z128xxx4_i2c_Manual.c
@@ -31,29 +31,19 @@
 // I2C READ ADDRESS
 //#define I2C_MAX3010_READ_ADDRESS (0xAB)
 //I2C WRITE ADDRESS
-//#define I2C_MAX3010_WRITE_ADDRESS (0xAA)
+#define I2C_MAX32664_SLAVE_ADDRESS (0xAA)
+#define READ_HUB_SENSOR_STATUS_REG (0x43)
 ////////////////////////////////////////////////////////////////////////////////
-#define GY521_SLAVE_ADDRESS (0xD0)
-#define WHO_AM_I (0x75)
-#define INT_PIN_CFG (0x37)
+//#define GY521_SLAVE_ADDRESS (0xD0)
+//#define WHO_AM_I (0x75)
+//#define INT_PIN_CFG (0x37)
 /*
  *****************************************************************************
-#define EXAMPLE_I2C_MASTER_BASEADDR I2C0
-#define I2C_MASTER_CLK_SRC I2C0_CLK_SRC
-#define I2C_MASTER_CLK_FREQ CLOCK_GetFreq(I2C0_CLK_SRC)
-#define I2C_MASTER_SLAVE_ADDR_7BIT 0x7EU
-#define I2C_BAUDRATE 100000U
-#define I2C_DATA_LENGTH 1U
 ////////////////////////////////////////////////////////////////////////////////
 // MAX32644 BIOMETRIC HUB
 // MAX32664GWEA
-// I2C ADDRESS
-#define I2C_MAX32644_ADDRESS 0x55
-
-
-
+ *
  //Code
- //
 */
 void delay(unsigned int length_ms)
 {
@@ -98,7 +88,7 @@ void Init_MAX32664(void)
 		//After an additional 50ms has elapsed, the MAX32664 is in application mode and the application performs its initialization of the application software
 		delay(50);
 		PRINTF("MAX32664 is in application mode, congratulations\r\n");
-		delay(1000);
+		delay(2000);
 		PRINTF("MAX32664 is ready to accept I2C commands\r\n");
 }
 
@@ -153,22 +143,65 @@ uint8_t i2c_read_byte(uint8_t slaveAddress, uint8_t slaveRegister){
 	return data;
 }
 
+
+uint8_t i2c_read_bytes(uint8_t slaveAddress, uint8_t familyByte, uint8_t indexByte ,uint8_t *data, uint8_t data_count){
+	uint8_t dummy;
+	int8_t num_bytes_read = 0;
+	I2C_TRAN; 								/*set to transmit mode*/
+	I2C_M_START; 							/*send start*/
+	I2C1->D = slaveAddress;					/*send device address (write)*/
+	I2C_WAIT;								/*wait for completion*/
+	I2C1->D = familyByte;					/*send register address */
+	I2C_WAIT;								/*wait for completion*/
+	I2C1->D = indexByte;					/*send register address */
+	I2C_WAIT;								/*wait for completion*/
+	I2C_M_STOP;
+	delay(20);
+	I2C_M_START; 							/*repeated start*/
+	I2C1->D = slaveAddress|0x01; 			/*send device address (read)*/
+	I2C_WAIT;								/*wait for completion*/
+	I2C_REC;								/*set to receive mode*/
+	ACK;									/*tell HW to send ACK after read*/
+	dummy = I2C1->D;						/*dummy read to start I2C read*/
+	I2C_WAIT;								/*wait for completion*/
+	do{
+		ACK;								/*tell HW to send ACK after read*/
+		data[num_bytes_read++] = I2C1->D; 	/*read data*/
+		I2C_WAIT;							/*wait for completion*/
+	}while (num_bytes_read < data_count-2);
+	NACK;									/*tell HW to send NACK after read*/
+	data[num_bytes_read++] = I2C1->D;		/*read data*/
+	I2C_WAIT;								/*wait for completion*/
+	I2C_M_STOP;								/*send stop*/
+}
+
 int main(void){
-	uint8_t intPinValue = 0x25;
-    BOARD_InitPins();
+	//uint8_t intPinValue = 0x25;    BOARD_InitPins();
     BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
 
 	Init_MAX32664();
 	Init_i2c();
 	PRINTF("Start with the Fun....!!\r\n");
-	PRINTF("Read WHO_AM_I Register value\r\n");
-	PRINTF("WHO_AM_I value is: 0x%x\r\n",i2c_read_byte(GY521_SLAVE_ADDRESS, WHO_AM_I));
-	PRINTF("Write value to a register\r\n");
-	PRINTF("Value of written to INT_PIN_CFG Register 0x%x is: 0x%x\r\n",INT_PIN_CFG, intPinValue);
-	i2c_write_byte(0xD0, INT_PIN_CFG, intPinValue);
-	delay(3000);
-	PRINTF("INT_PIN_CFG Value read is: 0x%x\r\n",i2c_read_byte(GY521_SLAVE_ADDRESS, INT_PIN_CFG));
+	PRINTF("Read READ_HUB_SENSOR_STATUS_REG value\r\n");
+	uint8_t data[256];
+	uint8_t indexByte = 0x03;
+	i2c_read_bytes(I2C_MAX32664_SLAVE_ADDRESS,READ_HUB_SENSOR_STATUS_REG,indexByte,data, 256);
+	for (uint32_t i = 0U; i < 256; i++)
+	    {
+	        if (i % 8 == 0)
+	        {
+	            PRINTF("\r\n");
+	        }
+	        PRINTF("0x%2x  ", data[i]);
+	    }
+	    PRINTF("\r\n\r\n");
+	//PRINTF("WHO_AM_I value is: 0x%x\r\n",i2c_read_byte(GY521_SLAVE_ADDRESS, WHO_AM_I));
+	//PRINTF("Write value to a register\r\n");
+	//PRINTF("Value of written to INT_PIN_CFG Register 0x%x is: 0x%x\r\n",INT_PIN_CFG, intPinValue);
+	//i2c_write_byte(0xD0, INT_PIN_CFG, intPinValue);
+	//delay(3000);
+	//PRINTF("INT_PIN_CFG Value read is: 0x%x\r\n",i2c_read_byte(GY521_SLAVE_ADDRESS, INT_PIN_CFG));
 
 	while(1){
 		;
