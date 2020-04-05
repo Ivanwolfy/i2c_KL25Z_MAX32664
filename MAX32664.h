ddb1c4e9d4922741931bfd6da27c90b3a2ceac5c
ivanwolfy <57159606+Ivanwolfy@users.noreply.github.com>
2020-04-05 10:29:13 -0400

    Add files via upload

---
+++ MAX32664.h
@@ -0,0 +1,17 @@
+/*
+ * MAX32664.h
+ *
+ *  Created on: Apr 4, 2020
+ *      Author: ivancortes
+ */
+
+#ifndef MAX32664_H_
+#define MAX32664_H_
+
+#define DEVICE_MODE (0X02)
+#define READ_REGISTER (0x41)
+#define GET_ATTRIBUTES_OF_THE_AFE (0x42)
+#define DUMP_REGISTERS (0x43)
+#define IDENTITY (0xFF)
+
+#endif /* MAX32664_H_ */
