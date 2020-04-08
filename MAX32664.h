05cfe6094789806f17a2eaa18198870f1020069c
ivanwolfy <57159606+Ivanwolfy@users.noreply.github.com>
2020-04-07 21:58:23 -0400

    Update MAX32664.h

--- MAX32664.h
+++ MAX32664.h
@@ -7,11 +7,16 @@
 
 #ifndef MAX32664_H_
 #define MAX32664_H_
-
+//Registers
+#define READ_SENSOR_HUB_STATUS (0X00)
 #define DEVICE_MODE (0X02)
+#define READ_OUTPUT_FIFO (0X12)
 #define READ_REGISTER (0x41)
 #define GET_ATTRIBUTES_OF_THE_AFE (0x42)
 #define DUMP_REGISTERS (0x43)
+#define SENSOR_MODE_ENABLE (0x44)
+#define ALGORITHM_MODE_ENABLE (0x52)
 #define IDENTITY (0xFF)
+#define SET_OUTPUT_MODE (0x10)
 
 #endif /* MAX32664_H_ */
